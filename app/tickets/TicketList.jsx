3640efe97f741d2a91933fd221ebf409e2536a52
ayyesu <iamdanielkissiedu@gmail.com>
2023-11-27 01:31:42 +0000

    Added the Ticket Detail Page

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,4 +1,6 @@
-async function getTicket() {
+import Link from "next/link";
+
+async function getTickets() {
   const res = await fetch("http://localhost:4000/tickets", {
     next: {
       revalidate: 0,
@@ -9,17 +11,19 @@ async function getTicket() {
 }
 
 export default async function TicketList() {
-  const ticket = await getTicket();
+  const ticket = await getTickets();
 
   return (
     <>
       {ticket.map((ticket) => (
         <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
       {ticket.length === 0 && (
