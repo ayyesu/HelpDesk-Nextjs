5d1988c8777b3a11246bb688c2441dda207b2fce
ayyesu <iamdanielkissiedu@gmail.com>
2023-11-28 01:52:35 +0000

    Added Link to access ticket form

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,20 +1,21 @@
-import Link from "next/link";
-import Image from "next/image";
-import Logo from "./mypicture.jpeg";
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from './mypicture.jpeg';
 
 export default function NavBar() {
-  return (
-    <nav>
-      <Image
-        src={Logo}
-        alt="Helpdesk logo"
-        width={70}
-        quality={100}
-        placeholder="blur"
-      />
-      <h1>Helpdesk</h1>
-      <Link href="/">Dashboard</Link>
-      <Link href="/tickets">Tickets</Link>
-    </nav>
-  );
+    return (
+        <nav>
+            <Image
+                src={Logo}
+                alt='Helpdesk logo'
+                width={70}
+                quality={100}
+                placeholder='blur'
+            />
+            <h1>Helpdesk</h1>
+            <Link href='/'>Dashboard</Link>
+            <Link href='/tickets'>Tickets</Link>
+            <Link href='/tickets/create'>Create Ticket</Link>
+        </nav>
+    );
 }
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,6 +1,7 @@
 import {Suspense} from 'react';
 import TicketList from './TicketList';
 import Loading from '../loading';
+import Link from 'next/link';
 
 export default function Ticket() {
     return (
